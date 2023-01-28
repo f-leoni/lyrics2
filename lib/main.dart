297633a29fbaf2e71083e8ccc8fa52d508f6b60c
Francesco Leoni <fraleoni@gmail.com>
2023-01-28 13:27:18 +0100

    Minor fixes to messages and coding style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
