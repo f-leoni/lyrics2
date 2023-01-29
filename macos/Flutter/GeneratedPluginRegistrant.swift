e38ed02583fd8e0ac1f3083d957f471d1fe37713
f-leoni <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>
2023-01-29 18:03:41 +0100

    First iOS integration and compile

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import catcher
+import device_info_plus
+import package_info_plus
+import path_provider_macos
+import sqflite
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CatcherPlugin.register(with: registry.registrar(forPlugin: "CatcherPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
