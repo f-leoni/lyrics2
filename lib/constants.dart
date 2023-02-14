9380f3ac6071b1bcb997fcff894a0cc702cf3cbb
Francesco Leoni <fraleoni@gmail.com>
2023-02-14 22:19:25 +0100

    go_router implementation work

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+// Routes names
+const String rootRouteName = 'home';
+const String onboardingRouteName = 'onboarding';
+const String infoRouteName = 'info';
+const String favoritesRouteName = 'favorites';
+const String viewLyricRouteName = 'view-lyric';
+const String settingsRouteName = 'settings';
